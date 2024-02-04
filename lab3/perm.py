64903031df1b61c634b698653437b087e45c4b98
Sagynysh <elmira.omarova55@gmail.com>
2024-02-04 20:15:41 +0300

    changes

--- lab3/perm.py
+++ lab3/perm.py
@@ -10,9 +10,7 @@ def get_permutation(string, i=0):
         words[i], words[j] = words[j], words[i]
    	 
         get_permutation(words, i + 1)
-#vjyhvyvlvul
-#dydy
-#wtfgug
+
 
 users_input=str(input("enter a string"))
 get_permutation(users_input)
