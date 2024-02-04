e02fc1d03834069f913021957712077405201cb6
Sagynysh <elmira.omarova55@gmail.com>
2024-02-04 23:02:30 +0300

    has33

---
+++ lab3/has33.py
@@ -0,0 +1,7 @@
+def has_33(nums):
+    for i in range(len(nums)-1):
+        if nums[i] == 3 and nums[i+1] == 3:
+            return True
+    return False
+my_list=list(map(int, input("Enter numbers:"). split()))
+print(has_33(my_list))
