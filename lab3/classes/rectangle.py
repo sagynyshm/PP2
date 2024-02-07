976599227961b62965a8384e8e6ceca3906b4160
Sagynysh <elmira.omarova55@gmail.com>
2024-02-07 09:09:35 +0300

    rect

--- lab3/classes/rectangle.py
+++ lab3/classes/rectangle.py
@@ -1,9 +1,18 @@
-class Shape():    def __init__(self):
-        pass    def area(self):
-        return 0
-class Rectangle(Shape):    def __init__(self, length=0, width=0):
-        Shape.__init__(self)        self.length=length
-        self.width=width    def area(self):
-        return self.length*self.width
-a=int(input())b=int(input())
-r=Rectangle(a,b)print(r.area())
+class Shape():
+        def __init__(self):
+                pass
+        def area(self):
+                return 0
+
+class Rectangle(Shape):    
+        def __init__(self, length=0, width=0):
+                Shape.__init__(self)
+                self.length=length
+                self.width=width
+        def area(self):
+                return self.length*self.width
+
+a=int(input())
+b=int(input())
+r=Rectangle(a,b)
+print(Rectangle.area())
--- lab3/funct1/has33.py
+++ lab3/funct1/has33.py
@@ -3,5 +3,6 @@ def has_33(nums):
         if nums[i] == 3 and nums[i+1] == 3:
             return True
     return False
+    
 my_list=list(map(int, input("Enter numbers:"). split()))
 print(has_33(my_list))
