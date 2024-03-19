65d47fe748a7f1ef32ec1d392bf4a57c9f5e63b0
Sagynysh <elmira.omarova55@gmail.com>
2024-03-19 18:15:36 +0300

    added lab7

--- lab7/audio.py
+++ lab7/audio.py
@@ -1,25 +1,24 @@
 import pygame
 import os
 
-# Initialize Pygame
+
 pygame.init()
 
-# Set screen dimensions (not used for this example)
+
 screen_width = 400
 screen_height = 400
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Player")
 
-# Initialize Pygame mixer for audio playback
+
 pygame.mixer.init()
-# Load image
-image_path = "C:/Users/Asus/Desktop/mysongs/alienpic.jpg"  # Path to your image file
+
+image_path = "C:/Users/Asus/Desktop/mysongs/alienpic.jpg"  
 image = pygame.image.load(image_path)
 
 
 
-# Set up the music directory
 music_directory = "C:/Users/Asus/Desktop/mysongs"
 
 music_files = ['alien.mp3', 'close.mp3', 'volcano.mp3', 'wish You Back.mp3', 'streetlight.mp3', 'levanter.mp3']
@@ -34,7 +33,6 @@ music_images = {
     'levanter.mp3': 'levanterpic.jpg'
 }
 
-# Current index of the playing music
 current_track_index = 0
 
 # Load the first image
@@ -45,36 +43,35 @@ current_image = pygame.transform.scale(current_image, (screen_width, screen_heig
 pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track_index]))
 pygame.mixer.music.play()
 
-# Main loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:  # Play/Pause
+            if event.key == pygame.K_SPACE:  # play
                 if pygame.mixer.music.get_busy():
                     pygame.mixer.music.pause()
                 else:
                     pygame.mixer.music.unpause()
-            elif event.key == pygame.K_RIGHT:  # Next track
+            elif event.key == pygame.K_RIGHT:  # next
                 current_track_index = (current_track_index + 1) % len(music_files)
                 current_image = pygame.image.load(os.path.join(music_directory, music_images[music_files[current_track_index]]))
                 current_image = pygame.transform.scale(current_image, (screen_width, screen_height))
                 pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track_index]))
                 pygame.mixer.music.play()
 
-            elif event.key == pygame.K_LEFT:  # Previous track
+            elif event.key == pygame.K_LEFT:  # prev
                 current_track_index = (current_track_index - 1) % len(music_files)
                 current_image = pygame.image.load(os.path.join(music_directory, music_images[music_files[current_track_index]]))
                 current_image = pygame.transform.scale(current_image, (screen_width, screen_height))
                 pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track_index]))
                 pygame.mixer.music.play()
 
-    # Fill the screen with the current image
+    
     screen.blit(current_image, (0, 0))
     pygame.display.flip()
-# Quit Pygame 
+
 pygame.quit()
 
 
